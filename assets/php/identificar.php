873a2f97c33a7d15eb9e7d1c059046629435964c
Erik <erikpvc@gmail.com>
2021-05-15 01:25:31 +0200

    Arreglos en la manera de identificarse

--- assets/php/identificar.php
+++ assets/php/identificar.php
@@ -4,20 +4,17 @@ $usuario = strip_tags(addslashes($_POST['p_username']));
 $password = strip_tags(addslashes($_POST['p_password']));
 
 $dbCall = new Database();
-$userlist = $dbCall->setQuery("SELECT * FROM registroylogin WHERE email=$usuario");
+$userlist = $dbCall->setQuery("SELECT * FROM `registroylogin` WHERE `email`='$usuario'");
 
-$fila = mysqli_fetch_array($userlist);
-$passok = password_verify($password, $fila['password']);
-
-if($fila['email']==$usuario && $passok==true) {
-	
-	session_start();
-	$_SESSION['email']=$fila['email'];
-	$_SESSION['level']=$fila['level'];
-
-	header('Location: ../../index.php');
-
-}else{
-	header('Location: ../../index.php?mensaje=mensaje_error');
+foreach($userlist as $row) {
+	if($row['email'] == $usuario && password_verify($password, $row['password']) == true) {
+		session_start();
+		$_SESSION['email']=$row['email'];
+		$_SESSION['level']=$row['level'];
+		
+		header('Location: ../../index.php');
+	} else {
+		header('Location: ../../index.php?mensaje=mensaje_error');
+	}
 }
 ?>
